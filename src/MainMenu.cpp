45018cb73d991699eaca429589127bba3b056dae
Sofixtg <stgebre21@ole.augie.edu>
2024-12-08 15:22:17 -0600

    Update MainMenu.cpp

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -2,54 +2,6 @@
 #include "Attributes.hh"
 #include "GUI.hh"
 #include "Window.hh"
-
-#include <iostream>
-
-/**
- * Window class for the Main menu.
- * @author Kaleb Gebrehiwot and Sofonias Gebre
- */
-class MainMenu : AUGL::Window{
-    public:
-        MainMenu(AUGL::Point& p, int w, int h, const std::string& title):
-        Window(p,w,h,title),
-        header({100,100},"Chess"),
-        credit({100, 200}, "by Kaleb Gebrehiwot and Sofonias Gebre"),
-        localMultiP({100,300}, 150, 75, AUGL::Text("Local Multiplayer"), [](Address_t, Address_t userData){ static_cast<MainMenu*>(userData)->PlayLocalMultiplayer();}),
-        againstComp({300,300}, 150, 75, AUGL::Text("Computer"), [](Address_t, Address_t userData){ static_cast<MainMenu*>(userData)->PlayAgainstComputer();}){
-            
-            
-            header.setTextStyle(KG::Text::TextStyle_t::HELVETICA_BOLD);
-            header.setTextSize(42);
-            header.setTextColor(AUGL::Color::BLACK);
-            attach(header);
-            
-            credit.setTextStyle(KG::Text::TextStyle_t::HELVETICA_ITALIC);
-            credit.setTextSize(24);
-            credit.setTextColor(AUGL::Color::BLACK);
-            attach(credit);
-
-            attach(localMultiP);
-            attach(againstComp);
-
-        }
-
-    private:
-        KG::Text header;
-        KG::Text credit;
-        AUGL::Button localMultiP;
-        AUGL::Button againstComp;
-
-        void PlayLocalMultiplayer(){
-            // Add funcitionality here
-        }
-        void PlayAgainstComputer(){
-            // Add functionality here
-        }
-};#include "Text.hh"
-#include "Attributes.hh"
-#include "GUI.hh"
-#include "Window.hh"
 #include <FL/Fl.H> 
 #include <FL/FL_draw.H> 
 #include <FL/FL_Widget.H> 
